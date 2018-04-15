d6d8c4b52375e11795c258b5f3b88b197bf8983b
AlisonZ <alisonkzerbe@gmail.com>
2018-04-15 15:46:24 -0700

    finds valid words from permutation list, does not account for blank tiles

--- lib/score.rb
+++ lib/score.rb
@@ -1,10 +1,21 @@
 
 # TODO: add class structure
+# put this dictionary hash creator in the class for starting new game of Scrabble
+def create_dictionary_hash
+  dictionary = {}
+  File.open("../data/dictionary.txt", "r") do |f|
+    f.each_line do |line|
+      dictionary[line.strip] = true;
+    end
+  end
+  return dictionary
+end
 
 def find_permutations(letters)
-
+  letters.downcase!
   perms_arr = []
   letters_arr = letters.split("")
+  # puts perms_arr
 
   # start with 2 letter words and continue to find perms up to number of letters
   i = 2
@@ -25,9 +36,19 @@ def find_permutations(letters)
 end
 
 
-def validate_words(arr)
-  puts arr
+def validate_words(perms_arr)
+  dictionary = create_dictionary_hash
+  valid_words = []
+
+  perms_arr.each do |word|
+    if dictionary[word]
+      valid_words.push(word)
+    end
+  end
+
+  puts valid_words
 end
+
 
 letters = "ARPNNLA"
 find_permutations(letters)
